233cf75d2fdb5b8aa7375c8ac33cd8ab5b496b49
Nazmul-nm <mdnazmul.nm@gmail.com>
2023-02-12 15:48:55 +0600

    lesson push static code push

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "../styles/App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+import Signup from "./pages/Signup";
+
 function App() {
   return (
-    <div className="App">
-      <div> Hello world</div>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/signup" element={<Signup />} exact />
+          <Route path="/login" element={<Login />} exact />
+          <Route path="/quiz" element={<Quiz />} exact />
+          <Route path="/result" element={<Result />} exact />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
